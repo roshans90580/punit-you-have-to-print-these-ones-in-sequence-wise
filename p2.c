96e3f6167cfb2e9a8a4019a0e3d8df511b0c9945
roshans90580 <roshans90580@gmail.com>
2025-11-03 17:18:49 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int num;
+    printf("enter your age :");
+    scanf("%d", &num);
+    printf("your age is %d\n",num);
+    printf("enter your height :");
+    scanf("%d", &num);
+    printf("your height is %d",num);
+    }
