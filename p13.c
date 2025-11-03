c0804658792b674f0e56b8d62705aeefb545a0d1
roshans90580 <roshans90580@gmail.com>
2025-11-03 19:50:45 +0530

    Create p13.c

---
+++ p13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main (){
+    int a;
+    printf("enter yerar");
+    scanf("%d",&a);
+    if(a%4==0){
+        printf("it is a leap year");
+    }else{
+        printf("it is not a leap year");
+    }
+}
